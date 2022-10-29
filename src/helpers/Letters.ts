a41cd01982ed590bafc906a4fc363584fe155678
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-10-29 11:11:19 -0500

    Estructura principal del proyecto

---
+++ src/helpers/Letters.ts
@@ -0,0 +1,18 @@
+export const letras: string[] = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+let palabras: string[] = [
+  "BONDAD",
+  "SINCERIDAD",
+  "EMPATIA",
+  "AMOR",
+  "PACIENCIA",
+  "GRATITUD",
+  "HUMILDAD",
+  "PERDON",
+  "RESPONSABILIDAD",
+  "SOLIDARIDAD"
+];
